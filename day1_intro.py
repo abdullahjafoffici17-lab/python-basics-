4164581cef059aa176d73ce9636553360a6ec451
abdullahjafoffici17-lab <abdullahjafoffici17@gmail.com>
2026-08-07 22:14:21 +0530

    Add files via upload

---
+++ day1_intro.py
@@ -0,0 +1,31 @@
+# this is a string variable
+print("Hello, I am learing python!")
+# this is a string variable
+name = "ABDUL AZEES"
+# this is an integer variable
+age = 16
+# this is a string variable
+school = "A/manarul uloom m.m.v"
+# this is a string variable
+hobby = "coding"
+# this is a string variable
+goal = "Ai Developer"
+# this is a string variable
+favorite_subject = "ICT and science"
+# this is a string variable
+colour = "Green"
+# this is an integer variable
+grade = 11
+# this is a  float variable
+weight = 40.5
+# this is a boolean variable
+likes_python = True
+
+
+
+
+
+print("My name is",name, "I am", age, "and study at", school)
+print("I love", hobby, "and I want to bocome a", goal)
+print("My favorite subject is", favorite_subject)
+print("I am grade", grade, "and my weight is", weight, "My favorite colour is", colour, "and I like python is", likes_python)
