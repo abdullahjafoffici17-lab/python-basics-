3a24faa2fbb2cc57ff4b153f130ffa3f2628ce5b
abdullahjafoffici17-lab <abdullahjafoffici17@gmail.com>
2026-08-14 16:42:16 +0530

    Add files via upload

---
+++ day9_unique_words.py
@@ -0,0 +1,9 @@
+skills =("code", "ai", "python", "ai", "code", "python")
+unique_skills = set(skills)
+print(skills)
+print(unique_skills)
+
+friend1_hobbies = {"code", "play cricket", "ai", "java learning"}
+friend2_hobbies = {"ai", "reading book", "code", "study maths"}
+print(friend1_hobbies.union(friend2_hobbies))
+print(friend1_hobbies.intersection(friend2_hobbies))
